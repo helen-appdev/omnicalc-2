9172930348833b27949e45174fcdf0c3c61451b2
helenavocado <hwang92@uchicago.edu>
2021-10-23 20:38:45 +0000

    hi

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-
+  get("/add", {:controller => "math", :action => "add_form"})
+  get("/wizard_add", {:controller => "math", :action => "add_results"})
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
