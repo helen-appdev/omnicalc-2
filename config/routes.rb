50691f81bced7bbcb52f2c0a43c0c4f25e0956ae
helenavocado <hwang92@uchicago.edu>
2021-10-23 21:18:30 +0000

    2

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   get("/wizard_add", {:controller => "math", :action => "add_results"})
   get("/subtract", {:controller => "math", :action => "subtract_form"})
   get("/wizard_subtract", {:controller => "math", :action => "subtract_results"})
+
+  get("/street_to_coords/new", {:controller => "umbrella", :action => "geocode_form"})
+  get("/street_to_coords/results", {:controller => "umbrella", :action => "geocode_results"})
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
